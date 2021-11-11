fd68d554bc9c4dfed89c4cce443f00b7982814e2
kimyujin <kimyujin@kimyujinui-MacBookAir.local>
2021-11-11 16:30:45 +0900

    first commit

---
+++ src/test/java/com/example/tika/ApacheTikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheTikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
